3a8d723b3279d481c301c862622c26aae4875a24
Eugene <supernovaprotocol@gmail.com>
2021-07-17 14:41:03 +0700

    Make sample command use new sampler class

--- sample.py
+++ sample.py
@@ -1,12 +1,11 @@
 import os
 import argparse
 import torch
-from handwriting_synthesis import data, utils, models
+from handwriting_synthesis.sampling import UnconditionalSampler
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("data_dir", type=str, help="Path to prepared dataset directory")
     parser.add_argument("path", type=str, help="Path to saved model")
     parser.add_argument("sample_dir", type=str, help="Path to directory that will contain generated samples")
     parser.add_argument(
@@ -19,21 +18,11 @@ if __name__ == '__main__':
 
     device = torch.device("cpu")
 
-    model = models.HandwritingPredictionNetwork.get_default_model(device, bias=args.bias)
-    model = model.to(device)
-    model.load_state_dict(torch.load(args.path, map_location=device))
-
-    with data.H5Dataset(f'{args.data_dir}/train.h5') as dataset:
-        mu = torch.tensor(dataset.mu)
-        sd = torch.tensor(dataset.std)
-
-    synthesizer = utils.HandwritingSynthesizer(
-        model, mu, sd, num_steps=args.steps, stochastic=True
-    )
-
     os.makedirs(args.sample_dir, exist_ok=True)
+
+    sampler = UnconditionalSampler.load(args.path, device, args.bias)
 
     for i in range(1, args.trials + 1):
         output_path = os.path.join(args.sample_dir, f'{i}.png')
-        synthesizer.synthesize(c=None, output_path=output_path, show_attention=False)
+        sampler.generate_handwriting(output_path=output_path)
         print(f'Done {i} / {args.trials}')
