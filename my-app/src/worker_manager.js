0f6cf8ddcc8658ae1f58177f44327a044901a627
Eugene <supernovaprotocol@gmail.com>
2021-09-29 20:45:08 +0700

    Fix a bug in worker

--- my-app/src/worker_manager.js
+++ my-app/src/worker_manager.js
@@ -4,12 +4,12 @@ onmessage = function(e) {
     if (e.data.event === "start") {
         if (worker === null) {
             worker = new Worker(new URL("./worker.js", import.meta.url));
-        }
 
-        let workerListener = e => {
-            self.postMessage(e.data);
+            let workerListener = e => {
+                self.postMessage(e.data);
+            }
+            worker.addEventListener('message', workerListener);
         }
-        worker.addEventListener('message', workerListener);
 
         let [text, bias, primingSequence, primingText] = e.data.params;
         worker.postMessage([text, bias, primingSequence, primingText]);
